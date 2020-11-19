801e39a46b29322121894f453e409c613b06151c
detroitgoose <73840991+detroitgoose@users.noreply.github.com>
2020-11-19 16:25:29 -0500

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install ubuntu-restricted-extras gnome-tweaks solaar firefox vlc gimp audacity -y
+sudo apt install ubuntu-restricted-extras gnome-tweaks solaar firefox vlc gimp speedtest-cli audacity -y
 
 sudo apt update
 
